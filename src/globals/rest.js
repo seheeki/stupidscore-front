7412b8ce02d754dc234cf4ae6a7e87a3f782d9a9
ksh <7262ksh@gmail.com>
2023-11-23 09:29:06 +0900

    rest.js

--- src/globals/rest.js
+++ src/globals/rest.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance2 = axios.create({
-	baseURL: "https://stupidscorewas.sehee.shop/course",
+	baseURL: "https://stupidscore.sehee.shop/course",
 });
 
 instance2.interceptors.request.use(function (config) {
